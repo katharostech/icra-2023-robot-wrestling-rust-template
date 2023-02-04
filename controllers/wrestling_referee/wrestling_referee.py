6f204b931a608407aff02048073a09182607e71d
Zicklag <zicklag@katharostech.com>
2023-02-04 09:09:38 -0600

    feat: update referee with latest version from main repository.

--- controllers/wrestling_referee/wrestling_referee.py
+++ controllers/wrestling_referee/wrestling_referee.py
@@ -28,8 +28,8 @@ class Referee (Supervisor):
                 self.digit[j][i] = self.getDevice('digit ' + str(j) + str(i))
         self.current_digit = [0, 0, 0]  # 0:00
         self.robot = [0] * 2
-        self.robot[0] = self.getFromDef('WRESTLER_RED')
-        self.robot[1] = self.getFromDef('WRESTLER_BLUE')
+        self.robot[0] = self.getFromDef('WRESTLER_RED').getFromProtoDef('HEAD_SLOT')
+        self.robot[1] = self.getFromDef('WRESTLER_BLUE').getFromProtoDef('HEAD_SLOT')
         self.min = [[0] * 3 for i in range(2)]
         self.max = [[0] * 3 for i in range(2)]
         for i in range(2):
@@ -59,6 +59,14 @@ class Referee (Supervisor):
         time = 0
         seconds = -1
         ko = -1
+        participant = os.environ['PARTICIPANT_NAME'] if 'PARTICIPANT_NAME' in os.environ else 'Participant'
+        opponent = os.environ['OPPONENT_NAME'] if 'OPPONENT_NAME' in os.environ else 'Opponent'
+        self.setLabel(0, '█' * 100, 0, 0, 0.1, 0xffffff, 0.3, 'Lucida Console')
+        self.setLabel(1, '█' * 100, 0, 0.048, 0.1, 0xffffff, 0.3, 'Lucida Console')
+        self.setLabel(2, participant, 0.01, 0.003, 0.08, 0xff0000, 0, 'Arial')
+        self.setLabel(3, opponent, 0.01, 0.051, 0.08, 0x0000ff, 0, 'Arial')
+        ko_labels = ['', '']
+        coverage_labels = ['', '']
         while True:
             if time % 200 == 0:
                 s = int(time / 1000) % 60
@@ -69,9 +77,10 @@ class Referee (Supervisor):
                 box = [0] * 3
                 for i in range(2):
                     position = self.robot[i].getPosition()
+                    color = 0xff0000 if i == 0 else 0x0000ff
                     if abs(position[0]) < 1 and abs(position[1]) < 1:  # inside the ring
                         coverage = 0
-                        for j in range(3):
+                        for j in range(2):
                             if position[j] < self.min[i][j]:
                                 self.min[i][j] = position[j]
                             elif position[j] > self.max[i][j]:
@@ -81,34 +90,45 @@ class Referee (Supervisor):
                         coverage = math.sqrt(coverage)
                         self.coverage[i] = coverage
                         self.indicator[i].setPosition(self.coverage[i] / 7)
-                    if position[2] < 0.75:  # low position threshold
+                        string = '{:.3f}'.format(coverage)
+                        if string != coverage_labels[i]:
+                            self.setLabel(4 + i, string, 0.8, 0.003 + 0.048 * i, 0.08, color, 0, 'Arial')
+                        coverage_labels[i] = string
+                    if position[2] < 0.9:  # low position threshold
                         self.ko_count[i] = self.ko_count[i] + 200
                         if self.ko_count[i] > 10000:  # 10 seconds
                             ko = i
                     else:
                         self.ko_count[i] = 0
-                if self.ko_count[0] > self.ko_count[1]:
-                    print(AnsiCodes.CLEAR_SCREEN)
-                    print('Red KO: %d' % (10 - self.ko_count[0] // 1000))
-                elif self.ko_count[1] > self.ko_count[0]:
-                    print(AnsiCodes.CLEAR_SCREEN)
-                    print('Blue KO: %d' % (10 - self.ko_count[1] // 1000))
+                    counter = 10 - self.ko_count[i] // 1000
+                    string = '' if self.ko_count[i] == 0 else str(counter) if counter > 0 else 'KO'
+                    if string != ko_labels[i]:
+                        self.setLabel(6 + i, string, 0.7 - len(string) * 0.01, 0.003 + 0.048 * i, 0.08, color, 0, 'Arial')
+                    ko_labels[i] = string
+
             if self.step(time_step) == -1 or time > game_duration or ko != -1:
                 break
             time += time_step
         if ko == 0:
-            print('performance:0' if CI else 'Red is KO. Blue wins!')
             print('Red is KO. Blue wins!')
+            performance = 0
         elif ko == 1:
-            print('performance:1' if CI else 'Blue is KO. Red wins!')
             print('Blue is KO. Red wins!')
-        # in case of coverage equality, red wins
-        elif self.coverage[0] >= self.coverage[1]:
-            print('performance:1' if CI else 'Red wins coverage: %s >= %s' % (self.coverage[0], self.coverage[1]))
-            print('Red wins coverage: %s >= %s' % (self.coverage[0], self.coverage[1]))
+            performance = 1
+        # in case of coverage equality, blue wins
+        elif self.coverage[0] > self.coverage[1]:
+            print('Red wins coverage: %s > %s' % (self.coverage[0], self.coverage[1]))
+            performance = 1
         else:
-            print('performance:0' if CI else 'Blue wins coverage: %s > %s' % (self.coverage[1], self.coverage[0]))
-            print('Blue wins coverage: %s > %s' % (self.coverage[1], self.coverage[0]))
+            print('Blue wins coverage: %s >= %s' % (self.coverage[1], self.coverage[0]))
+            performance = 0
+        self.setLabel(7 - performance, 'WIN', 0.673, 0.051 - 0.048 * performance,
+                      0.08, 0x0000ff if performance == 0 else 0xff0000, 0, 'Arial')
+        if CI:
+            self.step(3000)  # wait 3 seconds to display the result
+            self.animationStopRecording()  # stop the recording of the animation
+            self.step(time_step)
+            print(f'performance:{performance}')
 
 
 # create the referee instance and run main loop
