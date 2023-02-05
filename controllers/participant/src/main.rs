a1dd4d95debc93113af47c9798e4a05cb52d9c2b
Zicklag <zicklag@katharostech.com>
2023-02-05 15:08:01 -0600

    refactor: use default time step and account for it with loop counter.

--- controllers/participant/src/main.rs
+++ controllers/participant/src/main.rs
@@ -1,34 +1,37 @@
-use std::time::Duration;
-
 use webots::prelude::*;
 
+const MOVE_DELAY: u32 = 100;
+
 struct Participant {
+    loop_counter: u32,
     arm_up: bool,
     arm_motor: Motor,
 }
 
 impl Robot for Participant {
-    fn time_step(&self) -> std::time::Duration {
-        // Most robots will want this to something more like 16 milliseconds.
-        Duration::from_secs(1)
-    }
-
     fn init() -> Self {
         let arm_motor = Motor::new("RShoulderPitch");
         arm_motor.set_velocity(arm_motor.max_velocity());
         Participant {
             arm_motor,
+            loop_counter: 0,
             arm_up: false,
         }
     }
 
     fn step(&mut self) {
-        if !self.arm_up {
-            self.arm_motor.set_position(self.arm_motor.max_position());
-        } else {
-            self.arm_motor.set_position(self.arm_motor.min_position());
+        self.loop_counter += 1;
+
+        if self.loop_counter > MOVE_DELAY {
+            self.loop_counter = 0;
+
+            if !self.arm_up {
+                self.arm_motor.set_position(self.arm_motor.max_position());
+            } else {
+                self.arm_motor.set_position(self.arm_motor.min_position());
+            }
+            self.arm_up = !self.arm_up;
         }
-        self.arm_up = !self.arm_up;
     }
 }
 
